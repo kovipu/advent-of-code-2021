ecf242893536b631f0957c99fd9b769de5f3b71c
Konsta Purtsi <konsta@identio.fi>
2021-12-01 19:17:19 +0200

    Solve day 1 part 2.

--- src/day01.rs
+++ src/day01.rs
@@ -1,5 +1,6 @@
 pub fn part_1(input: &str) -> u32 {
-    let input: Vec<i32> = input.lines().map(|line| line.parse().unwrap()).collect();
+    let input: Vec<u32> = input.lines().map(|line| line.parse().unwrap()).collect();
+
     let mut acc = 0;
 
     input.iter().enumerate().for_each(|(i, x)| {
@@ -11,13 +12,43 @@ pub fn part_1(input: &str) -> u32 {
     acc
 }
 
+pub fn part_2(input: &str) -> u32 {
+    let input: Vec<u32> = input.lines().map(|line| line.parse().unwrap()).collect();
+
+    let windows = input.windows(3);
+
+    let i1 = &windows;
+    let i2 = &windows;
+
+    i1.clone()
+        .zip(i2.clone().skip(1))
+        .fold(0, |acc, (prev, cur)| {
+            if prev.iter().sum::<u32>() < cur.iter().sum::<u32>() {
+                acc + 1
+            } else {
+                acc
+            }
+        })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_part_1() {
-        let input = "199
+        let expected = 7;
+        assert_eq!(part_1(INPUT), expected);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let expected = 5;
+        assert_eq!(part_2(INPUT), expected);
+    }
+}
+
+const INPUT: &str = "199
 200
 208
 210
@@ -27,7 +58,3 @@ mod tests {
 269
 260
 263";
-
-        assert_eq!(part_1(input), 7);
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ mod day01;
 fn main() {
     println!("Running solution for day 1...");
 
-    let input = fs::read_to_string("inputs/01.txt")
-            .expect("Something went wrong reading the input file");
-    let output = day01::part_1(&input);
+    let input =
+        fs::read_to_string("inputs/01.txt").expect("Something went wrong reading the input file");
 
-    println!("Part 1 output: {}", output);
+    let output_1 = day01::part_1(&input);
+    println!("Part 1 output: {}", output_1);
+
+    let output_2 = day01::part_2(&input);
+    println!("Part 2 output: {}", output_2);
 }
