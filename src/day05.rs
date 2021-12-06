cef6a446c60c09308e3494992b940685204ae296
Konsta Purtsi <konsta@identio.fi>
2021-12-06 16:55:52 +0200

    Solve day 5 part 2.

--- src/day05.rs
+++ src/day05.rs
@@ -3,7 +3,10 @@ use regex::Regex;
 const MAP_SIZE: usize = 1000;
 
 pub fn part_1(input: &str) -> i32 {
-    let lines = input.lines().map(Vent::new).collect::<Vec<_>>();
+    let lines = input
+        .lines()
+        .map(|line| Vent::new(line, true))
+        .collect::<Vec<_>>();
 
     // Create a flat vector that has space for 2d stuff
     let mut map: Vec<i32> = vec![0; MAP_SIZE * MAP_SIZE];
@@ -18,7 +21,21 @@ pub fn part_1(input: &str) -> i32 {
 }
 
 pub fn part_2(input: &str) -> i32 {
-    0
+    let lines = input
+        .lines()
+        .map(|line| Vent::new(line, false))
+        .collect::<Vec<_>>();
+
+    // Create a flat vector that has space for 2d stuff
+    let mut map: Vec<i32> = vec![0; MAP_SIZE * MAP_SIZE];
+
+    for line in lines {
+        for point in line.points {
+            map[point.x + (point.y * MAP_SIZE)] += 1;
+        }
+    }
+
+    map.iter().filter(|x| *x > &1).count().try_into().unwrap()
 }
 
 #[derive(Debug)]
@@ -33,7 +50,7 @@ struct Point {
 }
 
 impl Vent {
-    fn new(line: &str) -> Self {
+    fn new(line: &str, ignore_diagonals: bool) -> Self {
         let re = Regex::new(r"([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)").unwrap();
         let caps = re.captures(line).unwrap();
 
@@ -43,19 +60,37 @@ impl Vent {
         let x2: usize = caps.get(3).unwrap().as_str().parse().unwrap();
         let y2: usize = caps.get(4).unwrap().as_str().parse().unwrap();
 
+        let xrange: Vec<usize> = if x1 < x2 {
+            (x1..=x2).collect()
+        } else {
+            (x2..=x1).rev().collect()
+        };
+
+        // These have no reason to be collected, I'm just bad at Rust.
+        let yrange: Vec<usize> = if y1 < y2 {
+            (y1..=y2).collect()
+        } else {
+            (y2..=y1).rev().collect()
+        };
+
         if x1 == x2 {
             // line is horizontal
-            let range = if y1 < y2 { y1..=y2 } else { y2..=y1 };
-            let points = range.map(|y| Point { x: x1, y }).collect();
+            let points = yrange.iter().map(|y| Point { x: x1, y: *y }).collect();
             Vent { points }
         } else if y1 == y2 {
             // line is vertical
-            let range = if x1 < x2 { x1..=x2 } else { x2..=x1 };
-            let points = range.map(|x| Point { x, y: y1 }).collect();
+            let points = xrange.iter().map(|x| Point { x: *x, y: y1 }).collect();
             Vent { points }
         } else {
             // line is diagonal
-            Vent { points: vec![] }
+            if ignore_diagonals {
+                return Vent { points: vec![] };
+            }
+
+            let range_combined = xrange.iter().zip(yrange);
+            let points: Vec<Point> = range_combined.map(|(x, y)| Point { x: *x, y }).collect();
+
+            Vent { points }
         }
     }
 }
@@ -67,6 +102,11 @@ mod tests {
     #[test]
     fn test_part_1() {
         assert_eq!(part_1(INPUT), 5);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(INPUT), 12);
     }
 }
 
