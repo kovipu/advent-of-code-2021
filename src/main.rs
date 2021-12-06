4ecd9a93af4b3d4f0eef48fe58a3adee26112fa2
Konsta Purtsi <konsta@identio.fi>
2021-12-06 16:31:33 +0200

    Solve day 5 part 1.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 fn main() {
-    for day in 1..=4 {
+    for day in 1..=5 {
         let input_filename = format!("inputs/{:02}.txt", day);
         let input = fs::read_to_string(input_filename).unwrap();
         let (part_1, part_2) = get_day(day);
@@ -19,12 +20,15 @@ fn main() {
     println!();
 }
 
-fn get_day(day: u32) -> (fn(&str) -> i32, fn(&str) -> i32) {
+fn get_day(day: u32) -> (DayFn, DayFn) {
     match day {
         1 => (day01::part_1, day01::part_2),
         2 => (day02::part_1, day02::part_2),
         3 => (day03::part_1, day03::part_2),
         4 => (day04::part_1, day04::part_2),
+        5 => (day05::part_1, day05::part_2),
         _ => panic!("Day {} not implemented", day),
     }
 }
+
+type DayFn = fn(&str) -> i32;
