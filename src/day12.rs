548cb15db5951885632f3b281583a9fdb3b2daaa
Konsta Purtsi <konsta@identio.fi>
2021-12-12 12:08:07 +0200

    Solve day 12 part 2.

--- src/day12.rs
+++ src/day12.rs
@@ -1,33 +1,35 @@
 use std::collections::HashMap;
 
 pub fn part_1(input: &str) -> i64 {
-    // I could use a bi-directional map for this.
-    let connections: HashMap<&str, Vec<&str>> =
-        input.lines().fold(HashMap::new(), |mut acc, line| {
-            let (a, b) = line.split_once('-').unwrap();
-
-            match acc.get_mut(a) {
-                Some(cons) => cons.push(b),
-                None => {
-                    acc.insert(a, vec![b]);
-                }
-            }
-
-            match acc.get_mut(b) {
-                Some(cons) => cons.push(a),
-                None => {
-                    acc.insert(b, vec![a]);
-                }
-            }
-
-            acc
-        });
-
+    let connections: HashMap<&str, Vec<&str>> = connections_hashmap(&input);
     find_paths(&connections, &[], "start").len() as i64
 }
 
 pub fn part_2(input: &str) -> i64 {
-    0
+    let connections: HashMap<&str, Vec<&str>> = connections_hashmap(&input);
+    find_paths_2(&connections, &[], "start", false).len() as i64
+}
+
+fn connections_hashmap(input: &str) -> HashMap<&str, Vec<&str>> {
+    input.lines().fold(HashMap::new(), |mut acc, line| {
+        let (a, b) = line.split_once('-').unwrap();
+
+        match acc.get_mut(a) {
+            Some(cons) => cons.push(b),
+            None => {
+                acc.insert(a, vec![b]);
+            }
+        }
+
+        match acc.get_mut(b) {
+            Some(cons) => cons.push(a),
+            None => {
+                acc.insert(b, vec![a]);
+            }
+        }
+
+        acc
+    })
 }
 
 fn find_paths<'a>(
@@ -50,6 +52,40 @@ fn find_paths<'a>(
         .iter()
         .fold(vec![], |mut acc, connection| {
             for path in find_paths(connections, &new_path, connection) {
+                acc.push(path);
+            }
+            acc
+        })
+}
+
+fn find_paths_2<'a>(
+    connections: &'a HashMap<&str, Vec<&str>>,
+    path: &[&'a str],
+    cave: &'a str,
+    cave_visited_twice: bool,
+) -> Vec<Vec<&'a str>> {
+    let mut new_path = path.to_owned();
+    new_path.push(cave);
+
+    if cave == "end" {
+        return vec![new_path];
+    } else if cave.to_lowercase() == *cave && path.contains(&cave) && cave_visited_twice {
+        return vec![];
+    }
+
+    let new_cave_visited_twice =
+        cave_visited_twice || cave.to_lowercase() == *cave && path.contains(&cave);
+
+    connections
+        .get(cave)
+        .unwrap()
+        .iter()
+        .fold(vec![], |mut acc, connection| {
+            if *connection == "start" {
+                return acc;
+            }
+
+            for path in find_paths_2(connections, &new_path, connection, new_cave_visited_twice) {
                 acc.push(path);
             }
             acc
